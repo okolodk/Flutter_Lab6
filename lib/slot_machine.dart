bcdd7cdbac16533cacce2cad5dcde4ec703f2d93
okolodk <grisha.zhivot@bk.ru>
2026-03-21 18:01:07 +0300

    Bonus: add sound effects via Web Audio API

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'slot_row.dart'; // <-- Добавьте эту строку среди других импортов
+import 'slot_row.dart'; 
+import 'sound_service.dart';
 class SlotMachine extends StatefulWidget {
   const SlotMachine({super.key});
 
@@ -24,7 +25,20 @@ class _SlotMachineState extends State<SlotMachine> {
   var _slot3 = 'assets/images/seven.png';
   var _message = '';
   var _isSpinning = false;
-
+  var _isMuted = false;
+  var _backgroundStarted = false;
+  void _toggleMute() {
+    setState(() {
+      _isMuted = !_isMuted;
+    });
+    SoundService.toggleMute();
+  }
+  @override
+  void initState() {
+    super.initState();
+    SoundService.playBackground();
+  }
+  
   Future<String> _spinReel({
     required int totalTicks,
     required void Function(String) onTick,
@@ -47,10 +61,15 @@ class _SlotMachineState extends State<SlotMachine> {
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning)  return;
+    SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
     });
+    if (!_backgroundStarted) {
+      SoundService.playBackground();
+      _backgroundStarted = true;
+    }
     final result1 = await _spinReel(
       totalTicks: 10, 
       onTick: (val) => setState(() => _slot1 = val)
@@ -70,13 +89,16 @@ class _SlotMachineState extends State<SlotMachine> {
         if (result1 == 'assets/images/seven.png') {
           _coins += 10;
           _message = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
+          SoundService.playJackpot();
         } else {
           _coins += 3;
           _message = 'Победа! 🎉 +3 монеты';
+          SoundService.playWin();
         }
       } else {
         _coins -= 1;
         _message = 'Попробуй ещё раз 😔 -1 монета';
+        SoundService.playLose();
       }
     });
   }
@@ -95,6 +117,19 @@ class _SlotMachineState extends State<SlotMachine> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Align(
+          alignment : Alignment.topRight,
+          child: Padding(
+            padding: EdgeInsets.only(right: 16, top: 8),
+            child: Icon(
+              _isMuted
+                ? Icons.volume_off
+                : Icons.volume_up,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
+        ),
         Text(
           '💰 Монеты: $_coins',
           style: TextStyle(
