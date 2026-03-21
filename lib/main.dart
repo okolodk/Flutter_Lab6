eb8d63e4cfb8f5024e03f2481a608bcd67d6e6b9
okolodk <grisha.zhivot@bk.ru>
2026-03-21 19:57:31 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:slot_machine6/slot_machine.dart';
+import 'package:slot_machine6/sound_service.dart';
 
-void main() {
+// Run | Debug | Profile
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SoundService.init();
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
-        body: SlotMachine()
-      ),
-    )
+        body: SlotMachine(),
+      ), // Scaffold
+    ), // MaterialApp
   );
 }
--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -61,7 +61,7 @@ class _SlotMachineState extends State<SlotMachine> {
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning)  return;
-    SoundService.playClick();
+    await SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
@@ -82,24 +82,30 @@ class _SlotMachineState extends State<SlotMachine> {
       totalTicks: 16, 
       onTick: (val) => setState(() => _slot3 = val)
     );
-    await Future.delayed(Duration(milliseconds: 300)); 
+    await Future.delayed(Duration(milliseconds: 300));
+    
+    String newMessage;
+    int coinsChange;
+    if (result1 == result2 && result2 == result3) {
+      if (result1 == 'assets/images/seven.png') {
+        coinsChange = 10;
+        newMessage = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
+        await SoundService.playJackpot();
+      } else {
+        coinsChange = 3;
+        newMessage = 'Победа! 🎉 +3 монеты';
+        await SoundService.playWin();
+      }
+    } else {
+      coinsChange = -1;
+      newMessage = 'Попробуй ещё раз 😔 -1 монета';
+      await SoundService.playLose();
+    }
+    
     setState(() {
       _isSpinning = false;
-      if (result1 == result2 && result2 == result3) {
-        if (result1 == 'assets/images/seven.png') {
-          _coins += 10;
-          _message = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
-          SoundService.playJackpot();
-        } else {
-          _coins += 3;
-          _message = 'Победа! 🎉 +3 монеты';
-          SoundService.playWin();
-        }
-      } else {
-        _coins -= 1;
-        _message = 'Попробуй ещё раз 😔 -1 монета';
-        SoundService.playLose();
-      }
+      _coins += coinsChange;
+      _message = newMessage;
     });
   }
   void _reset() {
